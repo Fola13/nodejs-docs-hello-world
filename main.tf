e6e7e5a6ef5d6e9e0ba62c70deb76e37a59e0c18
Fola13 <104541020+Fola13@users.noreply.github.com>
2023-04-17 23:48:02 +0930

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,10 +46,10 @@ resource "azurerm_linux_web_app" "webapp" {
 }
 
 #  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Fola13/nodejs-docs-hello-world"
-  branch             = "master"
-  use_manual_integration = true
-  use_mercurial      = false
-}
+#resource "azurerm_app_service_source_control" "sourcecontrol" {
+  #app_id             = azurerm_linux_web_app.webapp.id
+  #repo_url           = "https://github.com/Fola13/nodejs-docs-hello-world"
+  #branch             = "master"
+  #use_manual_integration = true
+  #use_mercurial      = false
+#}
